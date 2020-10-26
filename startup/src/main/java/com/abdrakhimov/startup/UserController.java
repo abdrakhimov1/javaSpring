1f7467b5f40b9c6a74ab244d6ddc8bdf4a38f7e3
shaldnikita <shaldnikita@yandex-team.ru>
2020-10-26 21:17:25 +0300

    asd

--- startup/src/main/java/com/abdrakhimov/startup/UserController.java
+++ startup/src/main/java/com/abdrakhimov/startup/UserController.java
@@ -5,20 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.naming.Binding;
 
-
-@RestController("users")
+@RestController
 public class UserController {
 
     @Autowired
     UserRepository repository;
 
-    @PostMapping("register")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     User newUser(@ModelAttribute("user") User user, Model model, BindingResult result) {
         return repository.save(user);
     }
